9ead71fb4b04df7636da61c6cb64e0d5e69c17ec
ah16165 <ah16165@bristol.ac.uk>
2019-10-14 17:15:41 +0100

    -Ofast and -march=native flags

--- stencil/compile.sh
+++ stencil/compile.sh
@@ -1,7 +1,7 @@
 rm stencil
 rm stencil.out
 rm stencil.pgm
-gcc -std=c99 -Wall stencil.c -o stencil
+gcc -std=c99 -Wall stencil.c -Ofast -march=native -o stencil
 sbatch stencil.job
 sleep 10
 python check.py --ref-stencil-file stencil_1024_1024_100.pgm --stencil-file stencil.pgm
--- stencil/compileintel.sh
+++ stencil/compileintel.sh
@@ -1,7 +1,7 @@
 rm stencil
 rm stencil.out
 rm stencil.pgm
-icc stencil.c -Ofast -o stencil
+icc stencil.c -Ofast -march=native -o stencil
 sbatch stencil.job
 sleep 10
 python check.py --ref-stencil-file stencil_1024_1024_100.pgm --stencil-file stencil.pgm
