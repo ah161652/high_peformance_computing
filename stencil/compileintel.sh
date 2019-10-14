c24dc92f22c9ca5d0c9a8ae7919789f0a9b988c9
ah16165 <ah16165@bristol.ac.uk>
2019-10-14 17:06:32 +0100

    added intel optimisation flag

--- stencil/compileintel.sh
+++ stencil/compileintel.sh
@@ -1,7 +1,7 @@
 rm stencil
 rm stencil.out
 rm stencil.pgm
-icc stencil.c -o stencil
+icc stencil.c -Ofast -o stencil
 sbatch stencil.job
 sleep 10
 python check.py --ref-stencil-file stencil_1024_1024_100.pgm --stencil-file stencil.pgm
