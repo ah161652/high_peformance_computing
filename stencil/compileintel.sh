20386136deb7c3f3220968722fb9ca2dcd864905
ah16165 <ah16165@bristol.ac.uk>
2019-10-14 16:48:22 +0100

    added script for intel compiler

--- stencil/compileintel.sh
+++ stencil/compileintel.sh
@@ -1,4 +1,4 @@
-gcc -std=c99 -Wall stencil.c -o stencil
+icc stencil.c -o stencil
 sbatch stencil.job
 sleep 10
 python check.py --ref-stencil-file stencil_1024_1024_100.pgm --stencil-file stencil.pgm
