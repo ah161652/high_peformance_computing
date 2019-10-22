b7f65a388e60215daa11ed4802aba2d0e49f2335
ah16165 <ah16165@bristol.ac.uk>
2019-10-22 14:55:00 +0100

    changed i/j

--- stencil/stencil.c
+++ stencil/stencil.c
@@ -67,8 +67,8 @@ void stencil(const int nx, const int ny, const int width, const int height,
 {
   float calc = 3.0/5.0;
   float calc2 = 0.5/5.0;
-  for (int j = 1; j < ny + 1; ++j) {
-    for (int i = 1; i < nx + 1; ++i) {
+  for (int i = 1; i < ny + 1; ++i) {
+    for (int j = 1; j < nx + 1; ++j) {
 
       tmp_image[j + i * height] =  image[j     + i       * height] * calc;
       tmp_image[j + i * height] += image[j     + (i - 1) * height] * calc2;
