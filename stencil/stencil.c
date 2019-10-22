5b930a10f5766e9e92dfe4c95f60cc1b9161a1d1
ah16165 <ah16165@bristol.ac.uk>
2019-10-22 17:12:24 +0100

    made 4 calcs, just 1 calc

--- stencil/stencil.c
+++ stencil/stencil.c
@@ -71,10 +71,10 @@ void stencil(const int nx, const int ny, const int width, const int height,
     for (int i = 1; i < nx + 1; ++i) {
 
       tmp_image[j + i * height] =  image[j     + i       * height] * calc;
-      tmp_image[j + i * height] += image[j     + (i - 1) * height] * calc2;
-      tmp_image[j + i * height] += image[j     + (i + 1) * height] * calc2;
-      tmp_image[j + i * height] += image[j - 1 + i       * height] * calc2;
-      tmp_image[j + i * height] += image[j + 1 + i       * height] * calc2;
+      tmp_image[j + i * height] += (image[j     + (i - 1) * height] * calc2) + (image[j     + (i + 1) * height] * calc2) + (image[j - 1 + i       * height] * calc2) + (image[j + 1 + i       * height] * calc2);
+      // tmp_image[j + i * height] += image[j     + (i + 1) * height] * calc2;
+      // tmp_image[j + i * height] += image[j - 1 + i       * height] * calc2;
+      // tmp_image[j + i * height] += image[j + 1 + i       * height] * calc2;
     }
   }
 }
