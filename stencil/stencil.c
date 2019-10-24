b0c6b67f30ed7916adabe057f4a48136aa10c045
Aidan Hood <ah16165@bristol.ac.uk>
2019-10-24 13:30:52 +0100

    final

--- stencil/stencil.c
+++ stencil/stencil.c
@@ -64,7 +64,7 @@ void stencil(const int nx, const int ny, const int width, const int height,
   for (int i = 1; i < ny + 1; ++i) {
     for (int j = 1; j < nx + 1; ++j) {
 
-      tmp_image[j + i * height] =  image[j     + i       * height] * calc + (image[j     + (i - 1) * height] * calc2) + (image[j     + (i + 1) * height] * calc2) + (image[j - 1 + i       * height] * calc2) + (image[j + 1 + i       * height] * calc2);
+      tmp_image[j + i * height] =  (image[j     + i       * height] * calc) + (image[j     + (i - 1) * height] * calc2) + (image[j     + (i + 1) * height] * calc2) + (image[j - 1 + i       * height] * calc2) + (image[j + 1 + i       * height] * calc2);
     //  tmp_image[j + i * height] += (image[j     + (i - 1) * height] * calc2) + (image[j     + (i + 1) * height] * calc2) + (image[j - 1 + i       * height] * calc2) + (image[j + 1 + i       * height] * calc2);
       // tmp_image[j + i * height] += image[j     + (i + 1) * height] * calc2;
       // tmp_image[j + i * height] += image[j - 1 + i       * height] * calc2;
