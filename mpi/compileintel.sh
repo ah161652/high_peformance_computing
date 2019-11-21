6b49878e7aec65ae7f983b1e6be500a7a7f8cc11
ah16165 <ah16165@bristol.ac.uk>
2019-11-21 11:42:53 +0000

    increased time

--- mpi/compileintel.sh
+++ mpi/compileintel.sh
@@ -3,6 +3,6 @@ rm stencil.out
 rm stencil.pgm
 mpiicc -std=c99 stencil.c -Ofast -xHOST -qopt-report=5 -qopt-report-phase=vec -o stencil
 sbatch stencil.job
-sleep 20
+sleep 40
 python check.py --ref-stencil-file stencil_1024_1024_100.pgm --stencil-file stencil.pgm
 cat stencil.out
