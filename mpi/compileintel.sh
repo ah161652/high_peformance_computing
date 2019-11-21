a3a3502a164b2d931bb63064f4ee1237a8c12163
ah16165 <ah16165@bristol.ac.uk>
2019-11-21 11:38:29 +0000

    updated script

--- mpi/compileintel.sh
+++ mpi/compileintel.sh
@@ -1,7 +1,7 @@
 rm stencil
 rm stencil.out
 rm stencil.pgm
-mpiicc stencil.c -Ofast -fast -xHOST -qopt-report=5 -qopt-report-phase=vec -o stencil
+mpiicc -std=c99 stencil.c -Ofast -fast -xHOST -qopt-report=5 -qopt-report-phase=vec -o stencil
 sbatch stencil.job
 sleep 10
 python check.py --ref-stencil-file stencil_1024_1024_100.pgm --stencil-file stencil.pgm
