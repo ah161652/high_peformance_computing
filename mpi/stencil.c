8223ebcc096fa3d2f5e485153a82b7e59ee4b3ab
ah161652 <aidan>
2019-12-05 11:40:47 +0000

    final image = image

--- mpi/stencil.c
+++ mpi/stencil.c
@@ -86,6 +86,8 @@ int main(int argc, char* argv[])
   // stitch it back together
   float* final_image = malloc(sizeof(float) * width * height);
 
+  final image  = image;
+
 
   // Output
   printf("------------------------------------\n");
