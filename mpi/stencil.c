06834d2d17906ed7167764efa15a38c02562f52c
ah161652 <aidan>
2019-12-05 11:41:18 +0000

    typo

--- mpi/stencil.c
+++ mpi/stencil.c
@@ -86,7 +86,7 @@ int main(int argc, char* argv[])
   // stitch it back together
   float* final_image = malloc(sizeof(float) * width * height);
 
-  final image  = image;
+  final_image  = image;
 
 
   // Output
