93b0c46161f33a5cf91939a62c9fc77644e6a5bc
ah161652 <aidan>
2019-12-08 17:21:41 +0000

    comments

--- mpi/stencil.c
+++ mpi/stencil.c
@@ -3,9 +3,11 @@
 #include <sys/time.h>
 #include <mpi.h>
 #include <math.h>
+
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
-//
+
+// Function declarations
 void stencil(const int nx, const int ny, const int width, const int height,
              float* image, float* tmp_image);
 void init_image(const int nx, const int ny, const int width, const int height,
@@ -14,20 +16,29 @@ void output_image(const char* file_name, const int nx, const int ny,
                   const int width, const int height, float* image);
 double wtime(void);
 void stencil_mpi(const int nx, const int ny, const int width, const int height,
-             float* image, float* tmp_image, int rank, int nprocs, int remainder_nx);
-void halo(int rank, float* image, int height, float* buff, int start_pxl, int nx_mpi, int section_size, int nprocs, int remainder_nx, int remainder_section_size);
+             float* image, float* tmp_image, int rank, int size, int remainder_nx);
+void halo(int rank, float* image, int height, int fist_pxl, int nx_mpi, int ncolumn_pxls, int size, int remainder_nx, int remainder_ncolumn_pxls);
 
 int main(int argc, char* argv[])
 {
+
+  //****** Initial Checks ******//
+
   // Check usage
   if (argc != 4) {
     fprintf(stderr, "Usage: %s nx ny niters\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  //MPI setup
+  //***********************************//
+
+
+
+  //****** MPI Setup ******//
+
+  //MPI init
   MPI_Init(&argc, &argv);
-  int nprocs, rank, flag;
+  int size, rank, flag;
 
   //Check if init worked
   MPI_Initialized(&flag);
@@ -35,8 +46,16 @@ int main(int argc, char* argv[])
     MPI_Abort(MPI_COMM_WORLD,EXIT_FAILURE);
   }
 
-  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  // MPI size and rank
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  //***********************************//
+
+
+
+
+  //****** Dimensions and Image allocation ******//
 
   // Initiliase problem dimensions from command line arguments
   int nx = atoi(argv[1]);
@@ -48,16 +67,21 @@ int main(int argc, char* argv[])
   int width = nx + 2;
   int height = ny + 2;
 
-  // Allocate the image
+  // Allocate and init the image
   float* image = malloc(sizeof(float) * width * height);
   float* tmp_image = malloc(sizeof(float) * width * height);
-
-
   init_image(nx, ny, width, height, image, tmp_image);
 
+  //***********************************//
 
-  // if 1 processor then run sequentially
-  if (nprocs == 1){
+
+
+
+  //****** Serial run for 1 core ******//
+
+// COULD USE A SERIAL FUNCTION
+  // if 1 core then run without mpi
+  if (size == 1){
 
 double tic = wtime();
  for (int t = 0; t < niters; ++t) {
@@ -66,7 +90,6 @@ double tic = wtime();
  }
  double toc = wtime();
 
- // Output
  printf("------------------------------------\n");
  printf(" runtime: %lf s\n", toc - tic);
  printf("------------------------------------\n");
@@ -78,94 +101,114 @@ double tic = wtime();
  return 0;
   }
 
-  // Split up columns
-  int nx_mpi = floor(nx/nprocs);
-  int remainder = nx % nprocs;
+  //***********************************//
+
+
+
+  //****** Define MPI related variables ******//
+
+  // Split up columns, and remainder column
+  int nx_mpi = floor(nx/size);
+  int remainder = nx % size;
   int remainder_nx = nx_mpi + remainder;
 
- printf("%d\n",nx_mpi);
+  // Define number of pixels in each column section
+  int ncolumn_pxls = height*nx_mpi;
+  int remainder_ncolumn_pxls = height * remainder_nx;
 
- int section_size = height*nx_mpi;
- int remainder_section_size = height * remainder_nx;
+  // Define the first pixel in each column section.
+  int fist_pxl = ((rank*nx_mpi)+1)*height;
 
- //define starting pixel
- int start_pxl = ((rank*nx_mpi)+1)*height;
+  //***********************************//
 
- float* buff = malloc(height*sizeof(float));
+
+
+  //****** Run stencil / halo exhange under MPI while timed ******//
 
   MPI_Barrier(MPI_COMM_WORLD);
-   // Call the stencil kernel under mpi
-
   double tic = wtime();
   for (int t = 0; t < niters; ++t) {
-    stencil_mpi(nx_mpi, ny, width, height, image, tmp_image, rank, nprocs, remainder_nx);
-    // MPI_Barrier(MPI_COMM_WORLD);
-    // printf("DEBUG1");
-    halo(rank, tmp_image, height, buff, start_pxl, nx_mpi, section_size, nprocs, remainder_nx, remainder_section_size);
-    //   MPI_Barrier(MPI_COMM_WORLD);
-    // printf("DEBUG2");
-    stencil_mpi(nx_mpi, ny, width, height, tmp_image, image, rank , nprocs, remainder_nx);
-    //   MPI_Barrier(MPI_COMM_WORLD);
-    // printf("DEBUG3");
-    halo(rank, image, height, buff, start_pxl, nx_mpi, section_size, nprocs, remainder_nx, remainder_section_size);
-    //   MPI_Barrier(MPI_COMM_WORLD);
-    // printf("DEBUG4");
+    stencil_mpi(nx_mpi, ny, width, height, image, tmp_image, rank, size, remainder_nx);
+
+    halo(rank, tmp_image, height, fist_pxl, nx_mpi, ncolumn_pxls, size, remainder_nx, remainder_ncolumn_pxls);
+
+    stencil_mpi(nx_mpi, ny, width, height, tmp_image, image, rank , size, remainder_nx);
+
+    halo(rank, image, height, fist_pxl, nx_mpi, ncolumn_pxls, size, remainder_nx, remainder_ncolumn_pxls);
+
   }
+  MPI_Barrier(MPI_COMM_WORLD);
   double toc = wtime();
 
-  //MPI_Barrier(MPI_COMM_WORLD);
-
-float* final_buff = malloc(sizeof(float)*section_size);
-float* remainder_final_buff = malloc(sizeof(float)*remainder_section_size);
+  //***********************************//
 
 
-if (rank == 0){
-  for(int i = 1; i < nprocs - 1; ++i){
-    MPI_Recv(final_buff, section_size, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    for (int j = 0; j< section_size; ++j){
-      image[(((nx_mpi*i)+1)*height) + j] = final_buff[j];
+  //****** Recombine image ******//
+
+
+  // COULD USE FINAL IMAGE INSTEAD
+  float* final_buff = malloc(sizeof(float)*ncolumn_pxls);
+  float* remainder_final_buff = malloc(sizeof(float)*remainder_ncolumn_pxls);
+
+  //Stitch function + switch statements
+  if (rank == 0){
+    for(int i = 1; i < size - 1; ++i){
+      MPI_Recv(final_buff, ncolumn_pxls, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      for (int j = 0; j< ncolumn_pxls; ++j){
+        image[(((nx_mpi*i)+1)*height) + j] = final_buff[j];
+      }
     }
+
+
+    MPI_Recv(remainder_final_buff, remainder_ncolumn_pxls, MPI_FLOAT, size-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    for (int j = 0; j< remainder_ncolumn_pxls; ++j){
+      image[(((nx_mpi*(size-1))+1)*height) + j] = remainder_final_buff[j];
+    }
+
   }
 
+  else if (rank == size -1){
+    for (int i = 0; i < remainder_ncolumn_pxls; i++) {
+      remainder_final_buff[i] = image[fist_pxl + i];
+    }
+    MPI_Send(remainder_final_buff ,remainder_ncolumn_pxls, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
 
-  MPI_Recv(remainder_final_buff, remainder_section_size, MPI_FLOAT, nprocs-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  for (int j = 0; j< remainder_section_size; ++j){
-    image[(((nx_mpi*(nprocs-1))+1)*height) + j] = remainder_final_buff[j];
   }
 
-
-}
-
-else if (rank == nprocs -1){
-  for (int i = 0; i < remainder_section_size; i++) {
-    remainder_final_buff[i] = image[start_pxl + i];
+  else{
+    for (int i = 0; i < ncolumn_pxls; i++) {
+      final_buff[i] = image[fist_pxl + i];
+    }
+    MPI_Send(final_buff,ncolumn_pxls, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
   }
-  MPI_Send(remainder_final_buff ,remainder_section_size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
 
-}
+  //***********************************//
 
-else{
-  for (int i = 0; i < section_size; i++) {
-    final_buff[i] = image[start_pxl + i];
+
+  //DELETE ONCE TESTED THAT ALL ARE SAME
+  printf("------------------------------------\n");
+  printf(" runtime: %lf s\n", toc - tic);
+  printf("------------------------------------\n");
+
+
+
+
+  //****** Output Image, time, and free memory ******//
+
+  if (rank == 0){
+    output_image(OUTPUT_FILE, nx, ny, width, height, image);
+
+    printf("------------------------------------\n");
+    printf(" FINAL runtime: %lf s\n", toc - tic);
+    printf("------------------------------------\n");
   }
-  MPI_Send(final_buff,section_size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
-}
-
-// Output
-printf("------------------------------------\n");
-printf(" runtime: %lf s\n", toc - tic);
-printf("------------------------------------\n");
-
-
-if (rank == 0){
-  output_image(OUTPUT_FILE, nx, ny, width, height, image);
-}
 
   free(image);
   free(tmp_image);
 
-
   MPI_Finalize();
+
+  //***********************************//
 
 }
 
@@ -186,7 +229,7 @@ void stencil(const int nx, const int ny, const int width, const int height,
 }
 
 void stencil_mpi(const int nx, const int ny, const int width, const int height,
-             float* image, float* tmp_image, int rank, int nprocs, int remainder_nx)
+             float* image, float* tmp_image, int rank, int size, int remainder_nx)
 {
   float calc = 3.0/5.0;
   float calc2 = 0.5/5.0;
@@ -195,6 +238,7 @@ void stencil_mpi(const int nx, const int ny, const int width, const int height,
   int end = start + nx;
   int remainder_end = start + remainder_nx;
 
+  //SWITCH STATEMENT
 
   if (rank ==0){
   for (int i = 1; i < end; ++i) {
@@ -206,7 +250,7 @@ void stencil_mpi(const int nx, const int ny, const int width, const int height,
   }
 }
 
-else if (rank == nprocs -1){
+else if (rank == size -1){
 
   for (int i = start; i < remainder_end; ++i) {
     for (int j = 1; j <  ny + 1; ++j) {
@@ -233,50 +277,62 @@ else{
 
 }
 
-void halo(int rank, float* image, int height, float* buff, int start_pxl, int nx_mpi, int section_size, int nprocs, int remainder_nx, int remainder_section_size)
+void halo(int rank, float* image, int height, int fist_pxl, int nx_mpi, int ncolumn_pxls, int size, int remainder_nx, int remainder_ncolumn_pxls)
 {
+
+//SWITCH STATEMENT
+float* buff = malloc(height*sizeof(float));
+
 if(rank == 0){
-  MPI_Sendrecv(&image[start_pxl + (nx_mpi-1)*height], height,  MPI_FLOAT, rank + 1, 0,
+//FIND FIRST HALO CELL VIA nx_mpi rather than first pxl
+  // Send right, receieve right, place in right halo column
+  MPI_Sendrecv(&image[fist_pxl + (nx_mpi-1)*height], height,  MPI_FLOAT, rank + 1, 0,
                buff, height, MPI_FLOAT, rank+1, 0,
                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
                 for (int i = 0; i < height; ++i) {
-                  image[start_pxl + section_size + i] = buff[i];
+                  image[fist_pxl + ncolumn_pxls + i] = buff[i];
                 }
 
 }
 
-else if ( rank == nprocs - 1){
-  MPI_Sendrecv(&image[start_pxl], height,  MPI_FLOAT, rank - 1, 0,
+else if ( rank == size - 1){
+
+  // Send left, recieve left, place in left halo column
+  MPI_Sendrecv(&image[fist_pxl], height,  MPI_FLOAT, rank - 1, 0,
                buff, height, MPI_FLOAT, rank-1, 0,
                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
                for (int i = 0; i < height; ++i) {
-                 image[start_pxl - height + i] = buff[i];
+                 image[fist_pxl - height + i] = buff[i];
 }
 }
 
 else {
-  MPI_Sendrecv(&image[start_pxl + ((nx_mpi - 1)*height)], height,  MPI_FLOAT, rank + 1, 0,
+
+  //Send left, receieve right, place in right halo column
+  MPI_Sendrecv(&image[fist_pxl], height,  MPI_FLOAT, rank - 1, 0,
+               buff, height, MPI_FLOAT, rank+1, 0,
+               MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+               for (int i = 0; i < height; ++i) {
+                 image[fist_pxl + ncolumn_pxls + i] = buff[i];
+               }
+
+  // Send right, receieve left, place in left halo column
+  MPI_Sendrecv(&image[fist_pxl + ((nx_mpi - 1)*height)], height,  MPI_FLOAT, rank + 1, 0,
                buff, height, MPI_FLOAT, rank-1, 0,
                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
                for (int i = 0; i < height; ++i) {
-                 image[start_pxl - height + i] = buff[i];
+                 image[fist_pxl - height + i] = buff[i];
                }
 
- MPI_Sendrecv(&image[start_pxl], height,  MPI_FLOAT, rank - 1, 0,
-              buff, height, MPI_FLOAT, rank+1, 0,
-              MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-              for (int i = 0; i < height; ++i) {
-                image[start_pxl + section_size + i] = buff[i];
-
 }
 
 
 }
-}
+
 
 
 // Create the input image
