b64ba41004ed58f3f127d73d8729634d912a4ae1
ah161652 <aidan>
2019-12-09 13:36:45 +0000

    c

--- mpi/stencil.c
+++ mpi/stencil.c
@@ -247,7 +247,7 @@ else if (rank != size -1){
 
 else{
 
-  for (int i = remainder_start; i < remainder_end; ++i) {
+  for (int i = start; i < remainder_end; ++i) {
     for (int j = 1; j <  ny + 1; ++j) {
 
       tmp_image[j + i * height] =  (image[j     + i       * height] * calc) + (image[j     + (i - 1) * height] * calc2) + (image[j     + (i + 1) * height] * calc2) + (image[j - 1 + i       * height] * calc2) + (image[j + 1 + i       * height] * calc2) ;
